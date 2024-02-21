469eb5640c0dbdc27a7293b995e536b8eb2deec2
Hibban Butt <48935944+hibbanbutt@users.noreply.github.com>
2024-02-20 23:35:01 -0700

    Update train_3.0.py
    Cleaned up old commented code.

--- train_3.0.py
+++ train_3.0.py
@@ -29,65 +29,6 @@ y = train['income>50K']
 X_kaggle = test.drop('ID', axis=1)
 id = test['ID']
 
-# Impute missing numerical values using the mean
-# numerical_imputer = SimpleImputer(strategy='mean')
-# X_numeric = X.select_dtypes(include=['number'])
-# X_numeric[:] = numerical_imputer.fit_transform(X_numeric)
-#
-# X_test_numeric =  X_test.select_dtypes(include=['number'])
-# X_test_numeric[:] = numerical_imputer.fit_transform(X_test_numeric)
-#
-# # Impute missing categorical values using the most frequent value
-# categorical_imputer = SimpleImputer(strategy='most_frequent')
-# X_categorical = X.select_dtypes(exclude=['number'])
-# X_categorical[:] = categorical_imputer.fit_transform(X_categorical)
-#
-# X_test_categorical = X_test.select_dtypes(exclude=['number'])
-# X_test_categorical[:] = categorical_imputer.fit_transform(X_test_categorical)
-#
-# print(X_categorical)
-# # Encode categorical values using one hot encoding
-# # encoder = OneHotEncoder(handle_unknown='ignore')
-# # X_categorical_encoded = encoder.fit_transform(X_categorical)
-# # X_test_categorical_encoded = encoder.transform(X_test_categorical)
-#
-# # Reconstruct the entire datasets, now encoded and complete
-# X_combined = pd.concat([X_numeric, X_categorical], axis=1, join='inner')
-# print(X_combined)
-# X_test_combined = pd.concat([X_test_numeric, X_test_categorical], axis=1, join='inner')
-
-# def objective(trial):
-#     cv_folder = StratifiedKFold(n_splits=5, random_state=1,shuffle=True)
-#
-#     model = CatBoostClassifier(
-#         iterations=trial.suggest_int("iterations", 100, 1000),
-#         learning_rate=trial.suggest_float("learning_rate", 1e-3, 1e-1, log=True),
-#         depth=trial.suggest_int("depth", 4, 10),
-#         l2_leaf_reg=trial.suggest_float("l2_leaf_reg", 1e-8, 100.0, log=True),
-#         bootstrap_type=trial.suggest_categorical("bootstrap_type", ["Bayesian"]),
-#         random_strength=trial.suggest_float("random_strength", 1e-8, 10.0, log=True),
-#         bagging_temperature=trial.suggest_float("bagging_temperature", 0.0, 10.0),
-#         od_type=trial.suggest_categorical("od_type", ["IncToDec", "Iter"]),
-#         od_wait=trial.suggest_int("od_wait", 10, 50),
-#         cat_features = X.select_dtypes(include=['object']).columns.tolist(),
-#         scale_pos_weight = len(y[y==0]) / len(y[y==1])
-#     )
-#
-#     return cross_val_score(model, X, y, cv=cv_folder, scoring="f1_weighted").mean()
-#
-#
-# sampler = TPESampler(seed=1)
-# study = optuna.create_study(study_name="catboost", direction="maximize", sampler=sampler)
-# study.optimize(objective, n_trials=100)
-#
-# print("Number of finished trials: ", len(study.trials))
-# print("Best trial:")
-# trial = study.best_trial
-# print("  Value: ", trial.value)
-# print("  Params: ")
-# for key, value in trial.params.items():
-#     print("    {} = {},".format(key, value))
-
 #Train
 model_final = CatBoostClassifier(    iterations = 995,
     learning_rate = 0.03460391162425298,
